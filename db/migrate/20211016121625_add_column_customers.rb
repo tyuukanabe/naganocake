bcd8c1c5f6131d45fbe731170f03705f0a65eaf6
fukusy <fuku1study@gmail.com>
2021-10-16 13:18:34 +0000

    [Add]routes追加

---
+++ db/migrate/20211016121625_add_column_customers.rb
@@ -0,0 +1,4 @@
+class AddColumnCustomers < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211016044431) do
+ActiveRecord::Schema.define(version: 20211016121625) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "customer_id"
