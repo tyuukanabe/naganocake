4f390f4294d92312c020970060851ef22edfbf61
kuroyuma <k.1993.yuma.0324@gmail.com>
2021-10-17 17:57:40 +0000

    [add]商品新規登録画面、詳細画面作成中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,9 +3,23 @@ class Admin::ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item)
+  end
+
   def show
-    
-  
+    @item = Item.find(params[:id])
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id, :item_name, :introduction, :price, :image, :is_status, )
+  end
+
 end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,11 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_items, dependent: :destroy
   belongs_to :genres
+  
+  
+# 消費税の計算
+  def add_tax_price
+    (self.price * 1.1).round
+  end
 
 end
