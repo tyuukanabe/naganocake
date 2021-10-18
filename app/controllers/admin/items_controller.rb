9ac903f7d5cccc7adccff2e1ca5be6035cf86c26
kuroyuma <k.1993.yuma.0324@gmail.com>
2021-10-18 06:45:39 +0000

    [fix]商品新規登録　[add]商品一覧表示、商品編集

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,12 +1,16 @@
 class Admin::ItemsController < ApplicationController
 
+  def index
+    @items = Item.page(params[:page]).per(10)
+  end
+
   def new
     @item = Item.new
   end
 
   def create
     @item = Item.new(item_params)
-    @item.save
+    @item.save!
     redirect_to admin_item_path(@item)
   end
 
@@ -16,6 +20,12 @@ class Admin::ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item.id)
   end
 
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,9 @@ class Item < ApplicationRecord
   attachment :image
   has_many :cart_items, dependent: :destroy
   has_many :order_items, dependent: :destroy
-  belongs_to :genres
-  
-  
+  belongs_to :genre
+
+
 # 消費税の計算
   def add_tax_price
     (self.price * 1.1).round
