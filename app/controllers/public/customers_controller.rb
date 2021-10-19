6d8e61382095bbc5b64e0d999850bdddac7aa610
maisoncnry12 <caxxry@gmail.com>
2021-10-19 03:10:24 +0000

    [Add]カート画面、マイページ作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,20 @@
 class Public::CustomersController < ApplicationController
+  def show
+      @customer = current_customer
+  end
+
+  def edit
+      @customer = current_customer
+  end
+
+  def update
+      @customer = current_customer
+      @customer.update(customer_params)
+       redirect_to current_customer
+  end
+
+  private
+  def customer_params
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :telephone, :email)
+  end
 end
