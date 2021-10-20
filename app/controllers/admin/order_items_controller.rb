f58f4699fc5f5d69881339ccc30669179a56881e
kuroyuma <k.1993.yuma.0324@gmail.com>
2021-10-20 12:08:32 +0000

    [add]商品詳細作成途中

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,2 +1,12 @@
 class Admin::OrderItemsController < ApplicationController
+    
+    def update
+        @order_item = OrderItem.find(params[:id])
+        @order_item.update(order_item_params)
+    end
+    
+    private
+    def order_item_params
+        params.require(:order_item).permit(:order_id, :item_id, :quantity, :purchase_price, :sales_order_status)
+    end
 end
