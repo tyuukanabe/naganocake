dfa9775b5ecc25a5580fe95ff62bca4acf813029
fukusy <fuku1study@gmail.com>
2021-10-20 07:39:20 +0000

    [Add]郵送先if文追加

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -27,24 +27,54 @@ class Public::OrdersController < ApplicationController
     end
   end
 
-  def show
-  end
 
   def confirm
+    @order = Order.new(order_params)
+
+    # 郵送先をif文で確認
+    if params[:order][:address_number] == "1"
+      @order.name = current_customer.full_name
+      @order.address = current_customer.customer_address
+    elsif params[:order][:address_number] == "2"
+      if Address.exists?(name: params[:order][:registered]).name
+        @order.name = Address.find(params[:order][:registered]).address
+      else
+        render :new
+      end
+    elsif params[:order][:address_number] == "3"
+      address_new = current_customer.addresses.new(address_params)
+      if address_new.save
+      else
+        render :new
+      end
+    else
+      redirect_to addresses_path
+    end
+
+
     @cart_items = CartItem.where(customer_id: @customer.id)
     # 全ての商品の税込価格×個数の合計
     @total_price = (@cart_items.map { |cart_item| cart_item.item.add_tax_price * cart_item.quantity }.sum ).floor
   end
 
-  def thanks
+  def show
+  end
+
+  def complete
   end
 
   def index
     @orders = Order.all
   end
 
+  private
+
   def order_params
-    params.require(:order).permit(:address_name, :address, :total_price)
+    params.require(:order).permit(:item_name, :address, :total_price)
+  end
+
+  def address_params
+    params.require(:order).permit(:name, :address)
   end
 
 
