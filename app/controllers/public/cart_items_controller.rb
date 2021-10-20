4ed0a861a414ee129c8f8411e57d63ca337229a0
maisoncnry12 <caxxry@gmail.com>
2021-10-20 04:46:01 +0000

    [Add]マイページ機能追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,31 +1,44 @@
 class Public::CartItemsController < ApplicationController
 
   def index
-
-    # @customer = current_customer
-    # @cart_items = CartItem.all
-
-
+    @cart_items = current_customer.cart_items
+    @sum = 0
   end
 
   def create
-    # @cart_item = cart_item.new(cart_item_params)
-    # @cart_items = current_customer.cart_items
+    @cart_items = current_customer.cart_items
+    @cart_item= CartItem.find_by(item_id: params[:cart_item][:item_id], customer_id: current_customer.id)
+    if @cart_item.blank?
+      @cart_item = CartItem.new(cart_item_params)
+    end
+    @cart_item.quantity += params[:cart_item][:quantity].to_i
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+    redirect_to cart_items_path
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update
+     redirect_to cart_items_path
   end
 
   def destroy
+    @cart_items = CartItem.find(params[:id])
+    @cart_items.destroy
+     redirect_to cart_items_path
   end
 
   def destroy_all
+    @cart_items = current_customer.cart_items
+    @cart_items.destroy_all
+     redirect_to cart_items_path
   end
 
   private
+
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :quantity)
-
   end
 
 end
