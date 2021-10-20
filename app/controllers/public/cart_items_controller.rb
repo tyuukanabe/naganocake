7d2a2e928f5bf250c77f2d67e6d4cfe2e27b6442
maisoncnry12 <caxxry@gmail.com>
2021-10-20 07:39:31 +0000

    [Add]カートページ作成中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,20 +6,23 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_items = current_customer.cart_items
-    @cart_item= CartItem.find_by(item_id: params[:cart_item][:item_id], customer_id: current_customer.id)
-    if @cart_item.blank?
-      @cart_item = CartItem.new(cart_item_params)
-    end
-    @cart_item.quantity += params[:cart_item][:quantity].to_i
-    @cart_item.customer_id = current_customer.id
+    @cart_item = current_customer.cart_items.new(cart_item_params)
     @cart_item.save
     redirect_to cart_items_path
+    # @cart_items = current_customer.cart_items
+    # @cart_item= CartItem.find_by(item_id: params[:cart_item][:item_id], customer_id: current_customer.id)
+    # if @cart_item.blank?
+    #   @cart_item = CartItem.new(cart_item_params)
+    # end
+    # @cart_item.quantity += params[:cart_item][:quantity].to_i
+    # @cart_item.customer_id = current_customer.id
+    # @cart_item.save
+    # redirect_to cart_items_path
   end
 
   def update
     @cart_item = CartItem.find(params[:id])
-    @cart_item.update
+    @cart_item.update(cart_item_params)
      redirect_to cart_items_path
   end
 
@@ -38,7 +41,7 @@ class Public::CartItemsController < ApplicationController
   private
 
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :quantity)
+    params.require(:cart_item).permit(:customer_id, :item_id, :quantity)
   end
 
 end
