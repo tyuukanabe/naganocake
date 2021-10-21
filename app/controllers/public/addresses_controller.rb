cb13418ee75098912164fb55f3c76f8b71ed74a1
kaichi0618 <kaichi.1998.0618@gmail.com>
2021-10-21 03:57:56 +0000

    [add]住所一覧

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -6,18 +6,26 @@ class Public::AddressesController < ApplicationController
   end
 
   def create
-    @addresses = Address.new(address_params)
-    @address.save
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @address.save!
     redirect_to addresses_path
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
   end
 
   def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
   end
 
   private
