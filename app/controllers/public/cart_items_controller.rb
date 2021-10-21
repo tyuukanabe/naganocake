639a1c4d35e8df25b6382f308203f108bc05de83
maisoncnry12 <caxxry@gmail.com>
2021-10-21 09:38:38 +0000

    [Add]カートページ作成中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -9,7 +9,7 @@ class Public::CartItemsController < ApplicationController
     @cart_item = current_customer.cart_items.new(cart_item_params)
      if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
        cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-       cart_item.quantity += params[:cart_item][:quantity].to_i
+       cart_item.quantity += params[:cart_item][:quantity]
        cart_item.save
      end
     @cart_item.save
@@ -26,14 +26,15 @@ class Public::CartItemsController < ApplicationController
   end
 
   def update
-    @cart_items= current_customer.cart_items
+    @cart_item= current_customer.cart_items.find_by(params[:id])
     @cart_item = @cart_items.find_by(item_id: params[:item_id])
     @cart_item.update(quantity: params[:quantity])
      redirect_to cart_items_path
   end
 
   def destroy
-    @cart_item = current_customer.cart_items.find(params[:id])
+    @cart_items = current_customer.cart_items
+    @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
      redirect_to cart_items_path
   end
