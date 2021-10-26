13c785614e9977e99290d03a5caaff7ac50d2b1e
kuroyuma <k.1993.yuma.0324@gmail.com>
2021-10-26 03:36:24 +0000

    [fix]新規商品バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,4 +16,12 @@ class Item < ApplicationRecord
     @items = Item.where("item_name LIKE ?", "%#{keyword}%")
   end
 
+
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :introduction, presence: true
+  validates :genre_id, presence: true
+  validates :price, presence: true
+  validates :is_status, presence: true
+
 end
